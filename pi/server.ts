94a74552db2aa8bd3da38e42b1e8dab922fb6e02
Michael Cook <michael.cook@thetechdept.com>
2019-12-12 17:14:35 +0000

    Simple server and GET reception

--- pi/server.ts
+++ pi/server.ts
@@ -1,31 +1,40 @@
+const fs = require("fs");
 const http = require("http");
+const ip = require("ip");
 const port = 55;
 
+// Start ngrok service for our http server
 const ngrok = require("ngrok");
 (async () => {
   const url = await ngrok.connect({
     port
   });
   console.log(
-    `LCD: Go to ${url.replace(
-      "https://",
-      ""
-    )} (perhaps use port 80 on pi if poss)`
+    `LCD: Go to ${url.replace("https://", "")}
+        or ${ip.address()}:${port}
+(Perhaps use port 80 on pi? ngrok still might be better. No same-wifi sitch)`
   );
 })().catch(e => {
   console.error("Error", e);
 });
 
 const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end("Hello Node.js Server!");
+  console.log("Request:", request.url);
+  const { url } = request;
+  if (url.startsWith(`/?authToken=`)) {
+    let token = url.replace("/?authToken=", "");
+    if (token) {
+      console.log("Sick, a token was submitted:", token);
+    }
+  }
+  const serverHtml = fs.readFileSync("./server.html", "utf8");
+  response.end(serverHtml);
 };
 
+// Start http server
 const server = http.createServer(requestHandler);
-
 server.listen(port, err => {
   if (err) {
-    return console.log("Something bad happened", err);
+    return console.log("Couldn't start a http server. Probs port issues:", err);
   }
-  console.log(`server is listening on ${port}`);
 });
